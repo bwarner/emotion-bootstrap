3d16bbd905b4f6e8d8184f8837be6841222bd459
Byron Warner <bfwarner@gmail.com>
2019-01-13 19:41:20 -0800

    Updated to Babel7

---
+++ babel.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "presets": [["@babel/env",
+    {
+      "useBuiltIns": "entry"
+    }
+  ]],
+  plugins: ['@babel/proposal-object-rest-spread'],
+};
+
--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,52 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
 const del = require('del');
-const sourcemaps = require('gulp-sourcemaps');
-
-gulp.task('default', ['build']);
 
 
-gulp.task('build-with-maps', () => {
+const plugins = [
+  ['@babel/proposal-object-rest-spread'],
+];
+
+const destination = 'dist';
+
+const babelConfig = {
+  sourceMap:true,
+  minified: true,
+};
+const babelTransform = babel(babelConfig);
+
+const buildWithMaps = (done) => {
+  gulp.src('vendor/*.js')
+  .pipe(babelTransform.on('error', console.log))
+  .pipe(gulp.dest(destination));
+
+  done();
+};
+
+
+function build (done) {
   gulp.src('vendor/**/*.js')
-  .pipe(sourcemaps.init())
-  .pipe(babel({
-    presets: ['env'],
-    plugins: [
-      ['transform-object-rest-spread',
-        { useBuiltIns: true }],
-    ],
-  }))
-  .pipe(sourcemaps.write('.'))
-  .pipe(gulp.dest('dist'));
+  .pipe(babelTransform)
+  .pipe(gulp.dest(destination));
+  done();
+}
 
-  gulp.src('vendor/**/*.js.map')
-  .pipe(gulp.dest('dist/vendor'));
-});
-
-gulp.task('build', () => {
-  gulp.src('vendor/**/*.js')
-  .pipe(babel({
-    presets: ['env'],
-    plugins: [
-      ['transform-object-rest-spread',
-        { useBuiltIns: true }],
-    ],
-  }))
-  .pipe(gulp.dest('dist'));
-
-});
-
-gulp.task('clean', (done) => {
+function clean(done) {
   del([`${__dirname}/dist/**`], done);
-});
+}
 
-gulp.task('watch', () => {
+function watch() {
   gulp.watch(['vendor/**/*.js', 'package.json'], function () {
     gulp.run('build');
   });
-});
+}
+
+gulp.task('default', gulp.series(build));
+
+gulp.task('build-with-maps', gulp.series(buildWithMaps));
+
+gulp.task('build', gulp.series(build));
+
+gulp.task('clean', gulp.series(clean));
+
+gulp.task('watch', gulp.series(watch));
